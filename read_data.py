7b836412e3da7f3da145ab791d04beb5fe4d91e6
Hannah Mowbray <hannahmowbray3@gmail.com>
2024-04-09 14:28:50 +0100

    started work on percentage change

---
+++ read_data.py
@@ -0,0 +1,64 @@
+import csv
+import pandas as pd
+
+
+def read_data(file_name): #reads csv file and outputs as a list of dictionaries where each key is column heading 
+    data = []
+
+    with open(file_name,'r') as my_csv:
+        spreadsheet = csv.DictReader(my_csv)
+        for row in spreadsheet:
+            data.append(row)
+
+    return data
+
+def max_values(heading): #extracts the max values for a column as a new dataframe
+    data = read_data('sales.csv')
+
+    df = pd.DataFrame(data) 
+    #converts the data into a pandas DataFrame
+
+    df2 = df[df[heading].values == df[heading].values.max()].reset_index(drop=True)
+    #makes a new dataframe from df with all of the values equal to the max value and resets the indices
+
+    return df2
+
+def min_values(heading): #extracts the min values for a column as a new dataframe
+    data = read_data('sales.csv')
+
+    df = pd.DataFrame(data) 
+    #converts the data into a pandas DataFrame
+
+    df2 = df[df[heading].values == df[heading].values.min()].reset_index(drop=True)
+    #makes a new dataframe from df with all of the values equal to the max value and resets the indices
+
+    return df2
+
+def print_max(heading): #takes the dataframe from max_values and prints each value
+    df2 = max_values(heading)
+    max_dates = []
+    for index, row in df2.iterrows():
+        max_value = row[heading]
+        max_date = str(row['month']) + ' ' + str(row['year'])
+        max_dates.append(max_date)
+        dates_str = str(max_dates)[1:-1]
+    print(f'Highest value of {heading}: {max_value}, corresponding date(s): {dates_str}')
+
+def print_min(heading): #takes the dataframe from min_values and prints each value
+    df2 = min_values(heading)
+    min_dates = []
+    for index, row in df2.iterrows():
+        min_value = row[heading]
+        min_date = str(row['month']) + ' ' + str(row['year'])
+        min_dates.append(min_date)
+        dates_str = str(min_dates)[1:-1]
+    print(f'Lowest value of {heading}: {min_value}, corresponding date(s): {dates_str}')
+
+print_max('expenditure')
+print_min('expenditure')
+
+print_max('sales')
+print_min('sales')
+    
+
+
