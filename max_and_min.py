a67ca4bf9b51f5efa86d3545c49c30383d990128
Hannah Mowbray <hannahmowbray3@gmail.com>
2024-04-10 19:14:38 +0100

    added return_max and return_min functions

--- max_and_min.py
+++ max_and_min.py
@@ -44,11 +44,35 @@ def print_min(file_name, heading): #takes the dataframe from min_values and prin
         dates_str = str(min_dates)[1:-1]
     print(f'Lowest value of {heading}: {min_value}, corresponding date(s): {dates_str}')
 
+def return_max(file_name, heading): #takes the dataframe from max_values and returns the max_value and date the max occurred
+    df2 = max_values(file_name, heading)
+    max_dates = []
+    for index, row in df2.iterrows():
+        max_value = row[heading]
+        max_date = str(row['month']) + ' ' + str(row['year'])
+        max_dates.append(max_date)
+        dates_str = str(max_dates)[1:-1]
+    return heading, max_value, dates_str
 
 
-print_max('sales.csv', 'expenditure')
-print_min('sales.csv', 'expenditure')
+def return_min(file_name, heading): #takes the dataframe from min_values and returns the min_value and date the min occurred
+    df2 = min_values(file_name, heading)
+    min_dates = []
+    for index, row in df2.iterrows():
+        min_value = row[heading]
+        min_date = str(row['month']) + ' ' + str(row['year'])
+        min_dates.append(min_date)
+        dates_str = str(min_dates)[1:-1]
+    return heading, min_value, dates_str
 
-print_max('sales.csv', 'sales')
-print_min('sales.csv', 'sales')
+
+# heading, min_value, dates_str = return_min('sales.csv', 'sales')
+# print(f'Lowest value of {heading}: {min_value}, corresponding date(s): {dates_str}')
+
+
+# print_max('sales.csv', 'expenditure')
+# print_min('sales.csv', 'expenditure')
+
+# print_max('sales.csv', 'sales')
+# print_min('sales.csv', 'sales')
     
