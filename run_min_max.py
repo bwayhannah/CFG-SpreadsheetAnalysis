b9af79378b55a55439e760255cfb38dd8c1f68ec
Hannah Mowbray <hannahmowbray3@gmail.com>
2024-04-19 19:40:07 +0100

    added convert to datetime

--- run_min_max.py
+++ run_min_max.py
@@ -1,17 +1,9 @@
-import csv
 import pandas as pd
+from read_data import read_data
 
-def read_data(file_name): #reads csv file and outputs as a list of dictionaries where each key is column heading 
-    data = []
 
-    with open(file_name,'r') as my_csv:
-        spreadsheet = csv.DictReader(my_csv)
-        for row in spreadsheet:
-            data.append(row)
-
-    return data
-
-def max_values(file_name, heading): #extracts the max values for a column as a new dataframe
+def return_max(file_name, heading, corresponds): #reads the data and returns the max_value and corresponding month/name the max occurred
+    # df2 = max_values(file_name, heading)
     data = read_data(file_name)
 
     df = pd.DataFrame(data) 
@@ -19,10 +11,16 @@ def max_values(file_name, heading): #extracts the max values for a column as a n
 
     df2 = df[df[heading].values == df[heading].values.max()].reset_index(drop=True)
     #makes a new dataframe from df with all of the values equal to the max value and resets the indices
+    max_names = []
+    for index, row in df2.iterrows(): #iterate over each row of the dataframe
+        max_value = row[heading] 
+        max_name = str(row[corresponds])
+        max_names.append(max_name)
+        names_str = str(max_names)[1:-1] #write as a string with slice to remove []
+    return heading, max_value, names_str
 
-    return df2
-
-def min_values(file_name, heading): #extracts the min values for a column as a new dataframe
+def return_min(file_name, heading, corresponds): #reads the data and returns the min_value and corresponding month/namethe min occurred
+    # df2 = min_values(file_name, heading)
     data = read_data(file_name)
 
     df = pd.DataFrame(data) 
@@ -31,28 +29,12 @@ def min_values(file_name, heading): #extracts the min values for a column as a n
     df2 = df[df[heading].values == df[heading].values.min()].reset_index(drop=True)
     #makes a new dataframe from df with all of the values equal to the min value and resets the indices
 
-    return df2
-
-
-def return_max(file_name, heading, corresponds): #takes the dataframe from max_values and returns the max_value and date the max occurred
-    df2 = max_values(file_name, heading)
-    max_names = []
-    for index, row in df2.iterrows():
-        max_value = row[heading]
-        max_name = str(row[corresponds])
-        max_names.append(max_name)
-        names_str = str(max_names)[1:-1]
-    return heading, max_value, names_str
-
-
-def return_min(file_name, heading, corresponds): #takes the dataframe from min_values and returns the min_value and date the min occurred
-    df2 = min_values(file_name, heading)
     min_names = []
-    for index, row in df2.iterrows():
-        min_value = row[heading]
+    for index, row in df2.iterrows(): #iterate over each row of the dataframe
+        min_value = row[heading] 
         min_name = str(row[corresponds])
         min_names.append(min_name)
-        names_str = str(min_names)[1:-1]
+        names_str = str(min_names)[1:-1] #write as a string with slice to remove []
     return heading, min_value, names_str
 
 
